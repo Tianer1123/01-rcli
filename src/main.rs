e4df933e896035be08033d1e41a8ee7fd1120c65
tianer <q2719833@126.com>
2024-12-31 12:50:57 +0800

    feature: add csv cli support

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,13 @@
 // rcli csv -i input.csv -o output.json --header -d ','
+
 use clap::Parser;
-
-#[derive(Parser, Debug)]
-#[command(name = "rcli", version, author, about, long_about = None)]
-struct Opts {
-    #[command(subcommand)]
-    cmd: SubCommand,
-}
-
-#[derive(Debug, Parser)]
-enum SubCommand {
-    #[command(name = "csv", about = "show CSV, convert CSV to other formats")]
-    Csv(CsvOpts),
-}
-
-#[derive(Debug, Parser)]
-struct CsvOpts {
-    #[arg(short, long)]
-    input: String,
-
-    #[arg(short, long, default_value = "output.json")] // "output.json".into()
-    output: String,
-
-    #[arg(short, long, default_value_t = ',')]
-    delimiter: char,
-
-    #[arg(long, default_value_t = true)]
-    header: bool,
-}
-
-fn main() {
+use rcli::{process_csv, Opts, SubCommand};
+fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     println!("{:?}", opts);
+    match opts.cmd {
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+
+    Ok(())
 }
---
+++ src/opts.rs
@@ -0,0 +1,38 @@
+use std::path::Path;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "rcli", version, author, about, long_about = None)]
+pub struct Opts {
+    #[command(subcommand)]
+    pub cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+pub enum SubCommand {
+    #[command(name = "csv", about = "show CSV, convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+pub struct CsvOpts {
+    #[arg(short, long, value_parser = verify_input)]
+    pub input: String,
+
+    #[arg(short, long, default_value = "output.json")] // "output.json".into()
+    pub output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    pub delimiter: char,
+
+    #[arg(long, default_value_t = true)]
+    pub header: bool,
+}
+
+fn verify_input(filename: &str) -> Result<String, String> {
+    match Path::new(filename).exists() {
+        true => Ok(filename.into()),
+        false => Err(format!("{} not exists", filename)),
+    }
+}
